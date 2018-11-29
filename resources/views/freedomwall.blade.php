86b2e8c75d3cb6efb990e25c29ba3e5ceacb5eef
jeromevillaver <jeromevillaver@gitlab.coredev.ph>
2018-11-29 11:14:51 +0800

    test2

--- resources/views/freedomwall.blade.php
+++ resources/views/freedomwall.blade.php
@@ -5,5 +5,9 @@
 		 <label for="lastname">Last Name:</label>
 		 {{ Form::textarea('notes', null, ['size' => '30x5'],['class'=>'form-control'] ) }}
 	</div>
+	<div class="row">
+		 <label for="firstname">First Name:</label>
+		 {{ Form::textarea('notes', null, ['size' => '30x5'],['class'=>'form-control'] ) }}
+	</div>
 </div>
 
