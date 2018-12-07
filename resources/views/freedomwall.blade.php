5f7cf459cfbbe623f0f1d9690557acbe85e839a8
jeromevillaver <jeromevillaver@gitlab.coredev.ph>
2018-12-07 10:46:12 +0800

    da

--- resources/views/freedomwall.blade.php
+++ resources/views/freedomwall.blade.php
@@ -16,6 +16,6 @@
 	<div class="row">
 		 <label for="contacts">Contacts:</label>
 		 {{ Form::textarea('notes', null, ['size' => '30x5'],['class'=>'form-control'] ) }}
-	</div>
+	</div>ad
 </div>
 sad
