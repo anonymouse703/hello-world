faa1fb7b51fa6c724765f03e1544f2a7c8c64229
jeromevillaver <jeromevillaver@gitlab.coredev.ph>
2018-12-07 10:37:04 +0800

    ada

--- resources/views/freedomwall.blade.php
+++ resources/views/freedomwall.blade.php
@@ -10,7 +10,7 @@
 		 {{ Form::textarea('notes', null, ['size' => '30x5'],['class'=>'form-control'] ) }}
 	</div>a
 	<div class="row">
-		 <label for="address">Address1:</label>
+		 <label for="address">Addresss1:</label>
 		 {{ Form::textarea('notess', null, ['size' => '30x5'],['class'=>'form-control'] ) }}
 	</div>
 	<div class="row">
